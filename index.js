5d66d908a6d99a738548e21cc9729b0a984f7bb8
prachiguptadev <117148255+prachiguptadev@users.noreply.github.com>
2022-11-04 16:10:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const array1 = ["1", "2", "3", "4"];
+const array2 = ["1", "2", "3", "4"];
+let a = Array.isArray(array1);
+let b = Array.isArray(array2);
+console.log(a);
+console.log(b);
+let c = a === b;
+console.log(c);
